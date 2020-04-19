70a18247c349b4d8459e876b6660c7fb5d614751
viu <vishnu.kraman88@gmail.com>
2020-04-20 02:46:14 +0530

    logout action

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -15,4 +15,12 @@ const mutation = {
   },
 };
 
-const action = {};
+const action = {
+  //mutations.setToken //bad!!
+  //don't call the mutations directly
+  //calling mutations we use the commit function
+  logout({ commit }) {
+    //calling mutations with the help of commit function and we pass two arguments mutations name and  the value that we want to pass
+    commit("setToken", null);
+  },
+};
