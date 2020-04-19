393bca9e962339eb194471773cfee5eb15dcfdc8
viu <vishnu.kraman88@gmail.com>
2020-04-20 02:35:41 +0530

    gettersobject of auth model

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,7 +2,12 @@ const state = {
   token: null,
 };
 
-const getters = {};
+const getters = {
+  isLoggedIn(state) {
+    //converting intp boolean
+    return !!state.token;
+  },
+};
 
 const action = {};
 
