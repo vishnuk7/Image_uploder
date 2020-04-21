ecc11d44d8da495e4bb02fee654834018ce179f7
viu <vishnu.kraman88@gmail.com>
2020-04-22 03:51:34 +0530

    improved ui

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="nav">
-    <div class="sidebar">
+  <div class="app">
+    <div class="nav">
       <div class="brand">
         <img src="../assets/photo.svg" alt="photo" />
       </div>
-      <div class="nav-items">
-        <div class="nav-item">
-          <a href>Gallery</a>
-        </div>
+
+      <div class="nav-items__login">
+        <a @click="login" href="#" class="nav-item__login">Login</a>
       </div>
     </div>
-    <div class="nav-items__login">
-      <a @click="login" href="#" class="nav-item">Login</a>
+    <div class="main">
+      <div class="sidebar">
+        <div class="nav-items">
+          <div class="nav-item">
+            <a href>Gallery</a>
+          </div>
+          <div class="nav-item">
+            <a href>Upload</a>
+          </div>
+          <div class="nav-item">
+            <a href>Logout</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,17 +42,22 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.main {
+  display: grid;
+  grid-template-columns: 14rem 1fr;
+}
+
 .nav {
   display: grid;
-  grid-template-columns: max-content 1fr;
-  grid-template-rows: 1fr 100vh;
-  font-size: 1.7rem;
+  grid-template-columns: 10rem 1fr;
   color: var(--text-color-1);
-  margin: 1rem 2rem;
+  background: #273042;
+  box-shadow: 2px 3px 4px #262626;
+  padding: 2rem 4rem;
 }
 
 .sidebar {
-  grid-row: 1/-1;
+  height: calc(100vh - 61px);
 }
 
 .brand img {
@@ -49,8 +65,38 @@ export default {
   /* height: 2rem; */
 }
 
+.nav-items {
+  height: 100%;
+  background: #273042;
+}
+
+.nav-item {
+  width: 100%;
+  height: calc(100% / 8);
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .nav-item a {
   color: var(--text-color-2);
+  text-decoration: none;
+  font-weight: 700;
+  font-size: 2rem;
+  width: inherit;
+  height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s;
+}
+
+.nav-item a:hover {
+  width: 100%;
+  height: 100%;
+  background: var(--color-dark-1);
+  /* transform: scale(1.2); */
 }
 
 .nav-items__login {
@@ -65,8 +111,9 @@ export default {
   padding: 0.7rem 2rem;
   color: var(--text-color-2);
   text-decoration: none;
-  font-weight: 500;
+  font-weight: 700;
   transition: all 0.3s;
+  font-size: 1.7rem;
 }
 
 .nav-items__login a:hover {
