62bea18094135ebee35c3dd6573009b0aba611d3
viu <vishnu.kraman88@gmail.com>
2020-04-24 03:15:42 +0530

    get request for all user images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from "qs";
+import axios from "axios";
 
 const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
 const ROOT_URL = "https://api.imgur.com";
@@ -13,5 +14,11 @@ export default {
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
       querystring
     )}`;
+  },
+
+  fetchImage(token) {
+    axios.get(`${ROOT_URL}/3/account/me/images`, {
+      Authorization: `Bearer ${token}`,
+    });
   },
 };
